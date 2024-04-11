660d417f0ec44e814f346334cd7efdadc828c75e
zhanghaibing <haibing.zhang@ly.com>
2024-04-11 20:39:40 +0800

    init

---
+++ src/main/java/com/lara/hellolara/HelloLaraApplication.java
@@ -0,0 +1,13 @@
+package com.lara.hellolara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloLaraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloLaraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lara/hellolara/HelloLaraApplicationTests.java
@@ -0,0 +1,14 @@
+package com.lara.hellolara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloLaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+		System.out.println("Hello Lara");
+	}
+
+}
